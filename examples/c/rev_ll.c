18c7844cf8a64a5394b1896b128426cdb84585b6
Rundle <mrundle@f45c89a55109.ant.amazon.com>
2018-07-28 02:01:01 -0700

    reverse linked list example

---
+++ examples/c/rev_ll.c
@@ -0,0 +1,42 @@
+/*
+ * Reverse a linked list
+ */
+
+#include <stdio.h>
+
+struct node {
+    int val;
+    struct node *next;
+};
+
+void print_list(struct node *node) {
+    while (node != NULL) {
+        printf("%d ", node->val);
+        node = node->next;
+    }
+    printf("\n");
+}
+
+struct node *reverse_list(struct node *node) {
+    if (node == NULL) return NULL;
+    struct node *prv = NULL;
+    struct node *cur = node;
+
+    while (cur != NULL) {
+        struct node *nxt = cur->next;
+        cur->next = prv;
+        prv = cur;
+        cur = nxt;
+    }
+    return prv;
+}
+
+int main(void) {
+    struct node d = { 4, NULL };
+    struct node c = { 3, &d   };
+    struct node b = { 2, &c   };
+    struct node a = { 1, &b   };
+
+    print_list(&a);
+    print_list(reverse_list(&a));
+}

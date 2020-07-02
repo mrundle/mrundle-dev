786cd658e128b828ba60fce55afd4278b43ac136
Matt Rundle <mrundle@amazon.com>
2020-07-02 05:58:22 +0000

    rewrite mergedict.py with doctests

---
+++ examples/python/_doctest.py
@@ -0,0 +1,1 @@
+mergedict.py
--- examples/python/mergedict.py
+++ examples/python/mergedict.py
@@ -2,147 +2,179 @@
 
 import copy
 import json
+import sys
 
-def jprint(x):
-    print(json.dumps(x, indent=4))
+from copy import deepcopy
+from enum import IntEnum, auto
+from doctest import testmod, master
+
+class MergeLevel(IntEnum):
+    '''
+    In merging a supplementary dict into a primary, the following values
+    represent different overwrite strategies that can be specified. Each
+    level is inclusive of the previous. See the doctests for the `merge`
+    function for examples.
+    '''
+    # No values allowed to be overwritten (default)
+    NONE   = auto()
+    # Allows overwrite of non-dict values for a matching key
+    LEAVES = auto()
+    # Allows overwrite of dicts for matching keys
+    TRUNKS = auto()
 
 # merge a supplementary dict into a main dict
-def merge(main, supp, overwrite_leaves=False, overwrite_trunks=False):
+def merge(main, supp, overwrite=MergeLevel.NONE):
     """
-    Merge a supplementary dict into a main dict, defaulting to no overwrites
-    on collisions. As a very simple example, consider the following:
+    Merge a supplementary dict into main dict, with a given overwrite level.
 
-        MAIN = {
-            'foo': {
-                'bar': 1,
-            },
-        }
-        SUPP = {
-            'foo': {
-                'baz': 1,
-            },
-        }
-        merge(MAIN, SUPP)
-        assert MAIN == {
-            'foo': {
-                'bar': 1,
-                'baz': 1,
-            },
-        }
+    Args:
+        main (dict): mutated via a merging in of `supp`
+        supp (dict): merged into `main`, and is not mutated
+        overwrite (enum MergeLevel): specification of merge overwrite level
 
-    Collisions are defined as cases where both dictionaries have matching
-    keys, but different values that are *not* dicts (matching keys with
-    dict values will result in a recursive merge call).
+    Return:
+        None: the `main` input dict is mutated directly
 
-    The following keyword arguments support overwrites on collisions:
+    Usage and Doctests:
 
-        - overwrite_leaves: In a collision where the main dicts "value"
-                            is NOT a dict, the new value from the supplementary
-                            dict will replace it.
+        The `doctest` module is employed below to exhibit working examples of usage
+        alongside descriptions of functionality.
 
-        - overwrite_trunks: In a collision where the main dicts "value"
-                            IS a dict, the new value from the supplementary
-                            dict will replace it. Note that overwrite_trunks
-                            implies overwrite_leaves.
+        At its most basic, the function will add new keys from `supp` into `main`.
+
+        >>> main = {
+        ...     'a': 0,
+        ... }
+        >>> supp = {
+        ...     'b': 0,
+        ... }
+        >>> merge(main, supp)
+        >>> main == {
+        ...     'a': 0,
+        ...     'b': 0,
+        ... }
+        True
+
+        When the main and supplementary dicts are found to share keys, and the value
+        of those keys is another dict, the merge will recurse. If the values of
+        matching keys differ in type, there are three supported options:
+
+            MergeLevel.NONE: only add new keys; never overwrite values
+            MergeLevel.LEAVES: overwrite values, but only non-dicts
+            MergeLevel.TRUNKS: overwrite values, including dicts
+
+        The following examples show practical implications of each option. The
+        following dictionaries represent the supplementary dict (`supp`), which
+        will be merged into `main`.
+
+        >>> main = {
+        ...     'a': {
+        ...         'a1': {
+        ...             'a2': {
+        ...                 'a3': 0,
+        ...             },
+        ...         },
+        ...     },
+        ...     'b': {
+        ...         'b1': 0,
+        ...     },
+        ... }
+        >>> supp = {
+        ...     'a': 'xyz',
+        ...     'b': {
+        ...         'b1': 1,
+        ...         'b2': 1,
+        ...     },
+        ... }
+
+        First, we'll consider the default, MergeLevel.NONE. Since no values are
+        allowed to be overwritten, the only change will be the addition of a brand
+        new key-value pair.
+
+        >>> _main = deepcopy(main)
+        >>> merge(_main, supp)
+        >>> assert _main == {
+        ...     'a': { # preserved by MergeLevel.NONE
+        ...         'a1': {
+        ...             'a2': {
+        ...                 'a3': 0,
+        ...             },
+        ...         },
+        ...     },
+        ...     'b': {
+        ...         'b1': 0, # preserved by MergeLevel.NONE
+        ...         'b2': 1, # new!
+        ...     },
+        ... }
+
+        Next, we'll consider MergeLevel.LEAVES. Here, non-dict keys, like strings,
+        integers, or arrays, can be overwritten. Since each merge level is inclusive
+        of the previous, changes from the last example will be made here too.
+
+
+        >>> _main = deepcopy(main)
+        >>> merge(_main, supp, MergeLevel.LEAVES)
+        >>> assert _main == {
+        ...     'a': { # preserved by MergeLevel.NONE
+        ...         'a1': {
+        ...             'a2': {
+        ...                 'a3': 0,
+        ...             },
+        ...         },
+        ...     },
+        ...     'b': {
+        ...         'b1': 1, # changed!
+        ...         'b2': 1, # new!
+        ...     },
+        ... }
+
+        Finally, we'll consider the most aggressive setting, MergeLevel.TRUNKS.
+        This allows the chopping off of dicts in the main blob when the supplementary
+        dict maps a non-dict value to a shared key.
+
+        >>> _main = deepcopy(main)
+        >>> merge(_main, supp, MergeLevel.TRUNKS)
+        >>> assert _main == {
+        ...     'a': 'xyz', # overwritten!
+        ...     'b': {
+        ...         'b1': 1, # changed!
+        ...         'b2': 1, # new!
+        ...     },
+        ... }
     """
-    oleaves = overwrite_leaves
-    otrunks = overwrite_trunks
 
-    # note: overwrite_trunks implies overwrite_trees
     for d in (main, supp):
         if not isinstance(d, dict):
-            print('nodict')
             return
 
     for k, v in supp.items():
-        # TODO if not gentle, overwrite values
         if k not in main:
+            """foo"""
             main[k] = v
         else:
-            # key match; recurse or collide
             if main[k] == v:
                 continue
             if isinstance(main[k], dict):
                 if isinstance(v, dict):
-                    # recurse
-                    merge(
-                        main[k],
-                        v,
-                        overwrite_leaves=oleaves,
-                        overwrite_trunks=otrunks)
-                elif otrunks:
+                    merge(main[k], v, overwrite)
+                elif overwrite == MergeLevel.TRUNKS:
                     main[k] = v
                 else:
                     continue
-            elif oleaves or otrunks:
+            elif overwrite >= MergeLevel.LEAVES:
                 main[k] = v
 
 
-def test_wrapper(func):
-    """
-    This wrapper function feeds a deepcopy of EXAMPLE_DICT into test functions,
-    which will call merge() and make their own assertions about expected output.
-    """
-    EXAMPLE_DICT = {
-        'foo': {
-            'bar': {
-                'baz': {
-                    'xyz': 23,
-                },
-            },
-        },
-        'bar': 9,
-    }
-    def wrapper():
-        main_dict = copy.deepcopy(EXAMPLE_DICT)
-        func(main_dict)
-    return wrapper
-
-@test_wrapper
-def test_basic_merge(main_dict):
-    new_dict = copy.deepcopy(main_dict)
-    new_dict['foo']['bar']['baz']['def'] = 12
-    merge(main_dict, new_dict)
-    b = {
-        'foo': {
-            'bar': {
-                'baz': {
-                    'xyz': 23,
-                    'def': 12,
-                },
-            },
-        },
-        'bar': 9,
-    }
-    assert main_dict == b
-
-@test_wrapper
-def test_overwrite_leaves(main_dict):
-    new_dict = {'bar': 12345}
-    merge(main_dict, new_dict, overwrite_leaves=True)
-    assert main_dict == {
-        'foo': {
-            'bar': {
-                'baz': {
-                    'xyz': 23,
-                },
-            },
-        },
-        'bar': 12345,
-    }
-
-@test_wrapper
-def test_overwrite_trunks(main_dict):
-    new_dict = {'foo': 10}
-    merge(main_dict, new_dict, overwrite_trunks=True)
-    assert main_dict == {
-        'foo': 10,
-        'bar': 9,
-    }
-
 if __name__ == '__main__':
-    test_basic_merge()
-    test_overwrite_leaves()
-    test_overwrite_trunks()
-    print('ok')
+    res = testmod(name='merge')
+    failed = res.failed
+    passed = res.attempted - res.failed
+    print('{}: {}/{} doctests passing'.format(
+        'ERROR' if failed > 0 else 'OK',
+        passed,
+        passed + failed
+    ))
+    # exit non-zero on any failure
+    sys.exit(failed)
+
 
